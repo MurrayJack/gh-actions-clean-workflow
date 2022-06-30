db7c83bb4752dee5fc051610fd4ddc5a4428e7e5
Murray Jack <mjack@starrez.com>
2022-06-30 19:51:48 +1000

    added loop

--- src/index.js
+++ src/index.js
@@ -18,57 +18,67 @@ async function run() {
      * https://octokit.github.io/rest.js/v18
      **/
     const octokit = new getOctokit(token);
+    let amount = 0
 
-    /**
-     * We need to fetch the list of workflow runs for a particular repo.
-     * We use octokit.paginate() to automatically loop over all the pages of the results.
-     */
-    // const workflows = await octokit.paginate(
-    //   "GET /repos/:owner/:repo/actions/workflows",
-    //   defaults
-    // );
-    const { data } = await octokit.rest.actions.listWorkflowRunsForRepo({
-      owner,
-      repo,
-      status: "completed",
-      per_page: 100,
-      created: `<${new Date(new Date().setDate(new Date().getDate() - numDaysOldToBeDeleted)).toISOString()}`
-    });
-
-    const hasRunBeforeDate = (run) => {
-      const diff = dateDiff(run.updated_at, Date.now());
-      return calcTimeUnits(diff).days >= numDaysOldToBeDeleted;
-    };
-
-    info(`${data.workflow_runs.length} total workflow runs`)
-
-    const workflowRunsToDelete = data.workflow_runs.filter(hasRunBeforeDate);
-
-    info(`${workflowRunsToDelete.length} workflow runs to be deleted`);
-
-    if (workflowRunsToDelete.length > 0) {
+    do {
       /**
-       * Loop over all the WorkflowRuns and delete them.
-       **/
-      const deleteRunAction = ({ id }) => {
-        info(`Deleting workflow run #${id}`);
+       * We need to fetch the list of workflow runs for a particular repo.
+       * We use octokit.paginate() to automatically loop over all the pages of the results.
+       */
+      // const workflows = await octokit.paginate(
+      //   "GET /repos/:owner/:repo/actions/workflows",
+      //   defaults
+      // );
+      const { data } = await octokit.rest.actions.listWorkflowRunsForRepo({
+        owner,
+        repo,
+        status: "completed",
+        per_page: 100,
+        created: `<${new Date(new Date().setDate(new Date().getDate() - numDaysOldToBeDeleted)).toISOString()}`
+      });
 
-        return octokit.rest.actions
-          .deleteWorkflowRun({ owner, repo, run_id: id })
-          .catch((err) => `An error occurrend: ${err.message}`);
+      const hasRunBeforeDate = (run) => {
+        const diff = dateDiff(run.updated_at, Date.now());
+        return calcTimeUnits(diff).days >= numDaysOldToBeDeleted;
       };
 
-      const requests = await Promise.all(
-        workflowRunsToDelete.map(deleteRunAction)
-      );
+      info(`${data.workflow_runs.length} total workflow runs`)
 
-      info(`${requests.length} workflow runs sucessfully deleted`);
+      const workflowRunsToDelete = data.workflow_runs.filter(hasRunBeforeDate);
 
-      setOutput(
-        "result",
-        `${requests.length} workflow runs sucessfully deleted`
-      );
-    }
+      amount = workflowRunsToDelete.length;
+      info(`${workflowRunsToDelete.length} workflow runs to be deleted`);
+
+      if (workflowRunsToDelete.length > 0) {
+        /**
+         * Loop over all the WorkflowRuns and delete them.
+         **/
+        const deleteRunAction = ({ id }) => {
+          info(`Deleting workflow run #${id}`);
+
+          return octokit.rest.actions
+            .deleteWorkflowRun({ owner, repo, run_id: id })
+            .catch((err) => `An error occurrend: ${err.message}`);
+        };
+
+        const requests = await Promise.all(
+          workflowRunsToDelete.map(deleteRunAction)
+        );
+
+        info(`${requests.length} workflow runs sucessfully deleted`);
+
+        setOutput(
+          "result",
+          `${requests.length} workflow runs successfully deleted`
+        );
+      }
+    } while (amount != 0)
+
+
+
+
+
+
   } catch (error) {
     setFailed(error.message);
   }
